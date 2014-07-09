97121c2b473905611890682dd95f87fa61abf786
Rob Day <Robert.day@dowjones.com>
2014-07-08 23:13:43 -0400

    Versioning all my dot files!

---
+++ .config/fish/config.fish
@@ -0,0 +1,7 @@
+#. ~/.config/fish/virtual.fish
+# set -g VIRTUALFISH_COMPAT_ALIASES # uncomment for virtualenvwrapper-style commands
+. ~/.config/fish/virtual.fish
+# optional plugins
+#. path/to/auto_activation.fish
+#. path/to/global_requirements.fish
+#. path/to/projects.fish
